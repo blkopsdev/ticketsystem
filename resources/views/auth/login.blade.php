5db5da2f96e48e53176b4f6ad25ba9c5b2c26759
blkopsdev <blkopsdev@gmail.com>
2018-06-07 14:24:39 -0400

    Implemented login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,50 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<div class="login">
+    <header>
+        <h1 class="text-center">{{ trans('translate.app_name') }}</h1>
+    </header>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <div class="authForm">
+        <h3 class="text-center">{{ trans('translate.log_in_to_your_account') }}</h3>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+        <form method="POST" action="{{ route('login') }}" class="validateJSForm form-signin">
+            @csrf
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+            <div class="form-group">
+                <label for="email" class="sr-only">{{ trans('translate.email') }}</label>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }} required autofocus" autocomplete="off" name="email" value="{{ old('email') }}" required autofocus placeholder="{{ trans('translate.email') }}">
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                @if ($errors->has('email'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('email') }}</strong>
+                    </span>
+                @endif
             </div>
-        </div>
+
+            <div class="form-group">
+                <label for="password" class="sr-only">{{ trans('translate.password') }}</label>
+                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }} required" autocomplete="off" name="password" required placeholder="{{ trans('translate.password') }}" data-parsley-minlength="6">
+
+                @if ($errors->has('password'))
+                    <span class="invalid-feedback">
+                        <strong>{{ $errors->first('password') }}</strong>
+                    </span>
+                @endif
+            </div>
+            <div class="form-group">
+                <button class="btn solso-email btn-block" type="submit">
+                    <i class="fa fa-sign-in"></i>{{ __('Login') }}
+                </button>
+            </div>
+            <div class="form-group">
+                <label><input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}</label>
+                <a class="pull-right" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+            </div>
+        </form>
     </div>
 </div>
+
 @endsection
